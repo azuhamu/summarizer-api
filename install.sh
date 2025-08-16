0f5167a76e74620788a9980c9f43ca7e1b0673a0
azuhamu <azuhamu@gmail.com>
2025-08-16 21:25:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,7 @@ set -e
 
 echo "📦 Installing Python dependencies with --prefer-binary..."
 
-# 最新の pip, setuptools, wheel にアップグレード
 pip install --upgrade pip setuptools wheel
-
-# requirements.txt を wheel 優先でインストール
 pip install --prefer-binary -r requirements.txt
 
 echo "✅ Installation complete."
